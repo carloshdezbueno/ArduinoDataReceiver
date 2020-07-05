e07b8a087b828997225320fbc65d542e278c53dd
Carlos <c.hdezbueno@gmail.com>
2020-07-05 21:44:22 +0200

    Receptor de datos funcionando

--- recogerDatosTFG.py
+++ recogerDatosTFG.py
@@ -2,6 +2,7 @@ import serial
 import os
 import time as t
 import requests as req
+import json
 
 comandos = {"posix": {'puertoSerie' : '/dev/ttyUSB0'},
             "nt": {'puertoSerie' : 'COM3'}}
@@ -11,14 +12,15 @@ arduino = serial.Serial(comandos[os.name]["puertoSerie"], 9600)
 
 print("Leyendo datos")
 
-def guardarDatos(texto):
-    peticion = req.post('http://localhost:8080/v1/insert', data = texto)
+def guardarDatos(datos):
+    jsonData = json.loads(datos)
+    peticion = req.post('http://localhost:8080/v1/insert', json=jsonData)
     print(peticion.status_code)
     
 
 while True:
     
     datos = arduino.readline()
-    guardarDatos(datos)
+    guardarDatos(datos.decode("utf-8") )
 
     t.sleep(30)
