04afaa389274e121190b197e63e468dea881696f
Carlos <c.hdezbueno@gmail.com>
2020-07-05 21:21:06 +0200

    cambio final

--- recogerDatosTFG.py
+++ recogerDatosTFG.py
@@ -1,37 +1,24 @@
 import serial
 import os
 import time as t
+import requests as req
 
-comandos = {"posix": {"ruta": '.', 'puertoSerie' : '/dev/ttyUSB0'},
-            "nt": {"ruta": 'D:/Drive/Universidad/4º Cuarto/Segundo semestre/TFG/TFG/Codigo/CodigoPython', 'puertoSerie' : 'COM3'}}
+comandos = {"posix": {'puertoSerie' : '/dev/ttyUSB0'},
+            "nt": {'puertoSerie' : 'COM3'}}
 
 arduino = serial.Serial(comandos[os.name]["puertoSerie"], 9600)
 
-cabecera = comandos[os.name]["ruta"] + "/datos"
-
-print("Guardando en: " + cabecera)
 
 print("Leyendo datos")
 
-def escribirFich(texto, ruta):
-
-    f = open(ruta, "ab")
-
-    if os.stat(ruta).st_size == 0:
-        f.write(b"[")
-    else:
-        f.seek(-1, 2)
-        f.truncate()
-        f.write(b",")
-    f.write(texto)
-    f.write(b"]")
-    f.close()
+def guardarDatos(texto):
+    peticion = req.post('http://localhost:8080/v1/insert', data = texto)
+    print(peticion.status_code)
+    
 
 while True:
-
-    ruta = cabecera + t.strftime("%m-%d-%Y") + ".json"
     
     datos = arduino.readline()
-    escribirFich(datos, ruta)
+    guardarDatos(datos)
 
     t.sleep(30)
